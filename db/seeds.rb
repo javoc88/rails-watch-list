161da325bdf77f70e02998edab015aedb68b8023
Javier Ochoa <javier.ochoa.77@gmail.com>
2021-08-03 12:25:28 -0400

    Controllers built

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Movie.destroy_all
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+puts "Creating movies"
+10.times do |i|
+  movies = JSON.parse(open("#{url}?page=#{i+1}").read)['results']
+  movies.each do |movie|
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create!(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
+end
+puts "Finished"
